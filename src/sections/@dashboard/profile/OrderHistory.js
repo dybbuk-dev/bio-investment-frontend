02b8e1944fd6b4e42564ac553dc5143d59d1c4d5
louis10271995 <louis10271995@gmail.com>
2023-03-06 15:58:43 +0200

    #add order history table

--- src/sections/@dashboard/profile/OrderHistory.js
+++ src/sections/@dashboard/profile/OrderHistory.js
@@ -1,6 +1,149 @@
-import { Button, Card, CardContent, CardHeader, Stack, Tab, Tabs, Typography } from "@mui/material";
 import { useState } from "react";
+import PropTypes from 'prop-types';
+import { Button, Card, CardContent, CardHeader, Checkbox, Stack, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel, Tabs, Typography } from "@mui/material";
+
 import useLocales from "../../../hooks/useLocales";
+
+EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+    checkbox: PropTypes.bool,
+};
+function EnhancedTableHead(props) {
+    const { checkbox, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+        props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {checkbox &&
+                    <TableCell padding="checkbox">
+
+                        <Checkbox
+                            color="primary"
+                            indeterminate={numSelected > 0 && numSelected < rowCount}
+                            checked={rowCount > 0 && numSelected === rowCount}
+                            onChange={onSelectAllClick}
+                            inputProps={{
+                                'aria-label': 'select all desserts',
+                            }}
+                        />
+                    </TableCell>
+                }
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+
+function createData(coin, amount, payment, date) {
+    return {
+      coin, amount, payment, date
+    };
+  }
+  
+  const rows = [
+   createData('Euro()',2400,'Visa',new Date()),
+   createData('Dollar()',2400,'Master Card',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+   createData('Real()',2400,'Pix',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+   createData('Real()',2400,'Visa',new Date()),
+  ];
+  
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+  
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+  
+ 
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  
+  const headCells = [
+    {
+      id: 'name',
+      numeric: false,
+      disablePadding: true,
+      label: 'Dessert (100g serving)',
+    },
+    {
+      id: 'calories',
+      numeric: true,
+      disablePadding: false,
+      label: 'Calories',
+    },
+    {
+      id: 'fat',
+      numeric: true,
+      disablePadding: false,
+      label: 'Fat (g)',
+    },
+    {
+      id: 'carbs',
+      numeric: true,
+      disablePadding: false,
+      label: 'Carbs (g)',
+    },
+    {
+      id: 'protein',
+      numeric: true,
+      disablePadding: false,
+      label: 'Protein (g)',
+    },
+  ];
+
 
 export default function OrderHistory() {
     const { translate } = useLocales();
@@ -13,19 +156,98 @@ export default function OrderHistory() {
             <Card>
                 <CardHeader title={translate('profile.order-history')} subheader={translate('profile.order-history-desc')} />
                 <CardContent>
-                    <Stack sx={{ borderBottomWidth: 1, borderBottomColor: 'divider', borderBottomStyle: 'solid' }} direction={{xs:'column', sm:'row'}} justifyContent={'space-between'}>
+                    <Stack sx={{ borderBottomWidth: 1, borderBottomColor: 'divider', borderBottomStyle: 'solid' }} direction={{ xs: 'column', sm: 'row' }} justifyContent={'space-between'}>
                         <Tabs value={selectedTab} onChange={onChange}>
                             <Tab value={"deposit"} label={translate('words.deposit')} />
                             <Tab value={"withdraw"} label={translate('words.withdraw')} />
                         </Tabs>
-                        <Stack direction="row" gap={1} pt={2} alignItems = {'center'}>
+                        <Stack direction="row" gap={1} pt={2} alignItems={'center'}>
                             <Typography variant={'body2'}>
                                 {translate('words.order-type')}
                             </Typography>
-                            <Button variant = 'contained' size = "small" sx = {{py:1}}>{translate('words.fiduciary')}</Button>
-                            <Button variant = 'outlined' size = "small"  sx = {{py:1}}>{translate('words.crypto')}</Button>
-                            
+                            <Button variant='contained' size="small" sx={{ py: 1 }}>{translate('words.fiduciary')}</Button>
+                            <Button variant='outlined' size="small" sx={{ py: 1 }}>{translate('words.crypto')}</Button>
+
                         </Stack>
+                    </Stack>
+                    <Stack>
+                        <TableContainer>
+                            <Table
+                                sx={{ minWidth: 750 }}
+                                aria-labelledby="tableTitle"
+                                size={dense ? 'small' : 'medium'}
+                            >
+                                <EnhancedTableHead
+                                    numSelected={selected.length}
+                                    order={order}
+                                    orderBy={orderBy}
+                                    onSelectAllClick={handleSelectAllClick}
+                                    onRequestSort={handleRequestSort}
+                                    rowCount={rows.length}
+                                    checkbox={false}
+                                />
+                                <TableBody>
+                                    {stableSort(rows, getComparator(order, orderBy))
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((row, index) => {
+                                            const isItemSelected = isSelected(row.name);
+                                            const labelId = `enhanced-table-checkbox-${index}`;
+
+                                            return (
+                                                <TableRow
+                                                    hover
+                                                    onClick={(event) => handleClick(event, row.name)}
+                                                    role="checkbox"
+                                                    aria-checked={isItemSelected}
+                                                    tabIndex={-1}
+                                                    key={row.name}
+                                                    selected={isItemSelected}
+                                                >
+                                                    <TableCell padding="checkbox">
+                                                        <Checkbox
+                                                            color="primary"
+                                                            checked={isItemSelected}
+                                                            inputProps={{
+                                                                'aria-labelledby': labelId,
+                                                            }}
+                                                        />
+                                                    </TableCell>
+                                                    <TableCell
+                                                        component="th"
+                                                        id={labelId}
+                                                        scope="row"
+                                                        padding="none"
+                                                    >
+                                                        {row.name}
+                                                    </TableCell>
+                                                    <TableCell align="right">{row.calories}</TableCell>
+                                                    <TableCell align="right">{row.fat}</TableCell>
+                                                    <TableCell align="right">{row.carbs}</TableCell>
+                                                    <TableCell align="right">{row.protein}</TableCell>
+                                                </TableRow>
+                                            );
+                                        })}
+                                    {emptyRows > 0 && (
+                                        <TableRow
+                                            style={{
+                                                height: (dense ? 33 : 53) * emptyRows,
+                                            }}
+                                        >
+                                            <TableCell colSpan={6} />
+                                        </TableRow>
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25]}
+                            component="div"
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
                     </Stack>
                 </CardContent>
             </Card>
